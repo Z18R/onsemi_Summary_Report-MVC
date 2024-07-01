109ae13a9a4c6ca4f44d1c4de99ef38e494a5198
Joe <gray78181878@gmail.com>
2024-07-01 18:55:32 +0800

    adding ATEC ossr this is for deployment

---
+++ ATEC ossr/onsemi shipping summary report/Models/DateFilterViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace onsemi_shipping_summary_report.Models
+{
+    public class DateFilterViewModel
+    {
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+    }
+
+}
---
+++ ATEC ossr/onsemi shipping summary report/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace onsemi_shipping_summary_report.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
